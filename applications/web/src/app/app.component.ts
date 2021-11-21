9744e25a8c8d2772985ea4d51265c6fee80abc25
Anton Hirov <hirov.anton@yahoo.com>
2021-11-21 18:05:14 +0200

    Added bootstrap and ngx-bootstrap

--- applications/web/src/app/app.component.ts
+++ applications/web/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, TemplateRef } from "@angular/core";
+import { BsModalRef, BsModalService } from "ngx-bootstrap/modal";
 
 @Component({
     selector: "el-root",
@@ -6,5 +7,11 @@ import { Component } from "@angular/core";
     styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-    title = "web";
+    modalRef?: BsModalRef;
+
+    constructor(private modalService: BsModalService) {}
+
+    openModal(template: TemplateRef<any>) {
+        this.modalRef = this.modalService.show(template);
+    }
 }
