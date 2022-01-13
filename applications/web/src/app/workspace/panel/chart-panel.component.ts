1742c17398749b6aa0a02e80087a4be0be9fc541
Anton Hirov <hirov.anton@yahoo.com>
2022-01-13 17:22:45 +0200

    Small fixes

--- applications/web/src/app/workspace/panel/chart-panel.component.ts
+++ applications/web/src/app/workspace/panel/chart-panel.component.ts
@@ -1,31 +1,6 @@
 import { Component } from "@angular/core";
-import { Mode, RoutingService } from "../workspace-routing.service";
 
 @Component({
-    /* ChartJs canvas element interferes with the routerLinkActive attribute */
-    template: `<div
-        class="
-        d-flex
-        flex-lg-column
-        align-items-center
-        justify-content-evenly
-        h-100
-    ">
-        <button
-            routerLink="../income"
-            [class.active]="modeService.savedMode === MODE.Income"
-            class="btn btn-outline-primary w-50 mx-4 my-3 my-lg-0 fw-bold">
-            Income
-        </button>
-        <button
-            routerLink="../outcome"
-            [class.active]="modeService.savedMode === MODE.Outcome"
-            class="btn btn-outline-primary w-50 mx-4 my-3 my-lg-0 fw-bold">
-            Outcome
-        </button>
-    </div>`,
+    templateUrl: "./panel.component.html",
 })
-export class ChartPanelComponent {
-    public MODE = Mode;
-    constructor(public modeService: RoutingService) {}
-}
+export class ChartPanelComponent {}
