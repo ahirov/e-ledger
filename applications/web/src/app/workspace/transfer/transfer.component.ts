7d401619dc2020c770c580a2de8f1ebeeb72ea74
Anton Hirov <hirov.anton@yahoo.com>
2022-01-15 02:06:27 +0200

    Added transfer page template

--- applications/web/src/app/workspace/transfer/transfer.component.ts
+++ applications/web/src/app/workspace/transfer/transfer.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 
 import { Subscription } from "rxjs";
+import { Extension } from "./model/extension.mode";
 import { Mode, RoutingService } from "../workspace-routing.service";
 
 @Component({
@@ -12,19 +13,21 @@ export class TransferComponent implements OnInit, OnDestroy {
     private _paramsSub!: Subscription;
 
     public MODE = Mode;
+    public extensions!: Extension[];
 
     constructor(
         private _route: ActivatedRoute,
         public modeService: RoutingService,
     ) {}
 
-    ngOnInit(): void {
+    public ngOnInit(): void {
         this._paramsSub = this._route.params.subscribe(params =>
             this.modeService.saveMode(params),
         );
+        this.extensions = [Extension.Xlsx, Extension.Csv];
     }
 
-    ngOnDestroy(): void {
+    public ngOnDestroy(): void {
         if (this._paramsSub) {
             this._paramsSub.unsubscribe();
         }
--- applications/web/src/app/workspace/transfer/transfer.module.ts
+++ applications/web/src/app/workspace/transfer/transfer.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
 
+import { NgSelectModule } from "@ng-select/ng-select";
+import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
 import { TransferComponent } from "./transfer.component";
 
 @NgModule({
     declarations: [TransferComponent],
-    imports: [CommonModule],
+    imports: [
+        CommonModule,
+        FormsModule,
+        NgSelectModule,
+        BsDatepickerModule.forRoot(),
+    ],
     exports: [TransferComponent],
 })
 export class TransferModule {}
