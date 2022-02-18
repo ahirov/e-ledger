bd1c675252f6651fc1d0c89cbdaaacd5874a69c1
Anton Hirov <hirov.anton@yahoo.com>
2022-02-18 14:53:10 +0200

    Removed duplicates

--- applications/web/src/app/workspace/shared/workspace-shared.module.ts
+++ applications/web/src/app/workspace/shared/workspace-shared.module.ts
@@ -2,9 +2,20 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { NgSelectModule } from "@ng-select/ng-select";
 import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
+import {
+    DatepickerDirective,
+    DatepickerInputDirective,
+} from "./directives/datepicker.directive";
 
 @NgModule({
+    declarations: [DatepickerDirective, DatepickerInputDirective],
     imports: [FormsModule, NgSelectModule, BsDatepickerModule.forRoot()],
-    exports: [FormsModule, NgSelectModule, BsDatepickerModule],
+    exports: [
+        FormsModule,
+        NgSelectModule,
+        BsDatepickerModule,
+        DatepickerDirective,
+        DatepickerInputDirective,
+    ],
 })
 export class WorkspaceSharedModule {}
